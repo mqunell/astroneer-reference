7a5bac46d86bf5014302f0a66c488bef8a14ee50
Matt <mqunell@proton.me>
2024-02-04 11:53:53 -0800

    start fresh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import '@/styles/globals.css';
+
+export const metadata: Metadata = {
+	title: 'Astroneer Reference',
+	description: 'Astroneer Reference',
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
 	<html lang="en">
